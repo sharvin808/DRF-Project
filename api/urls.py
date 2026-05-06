65928bf831409d7328c5c5dca299e523e97148cd
Sharvin Vincent <sharvinvincent9100@gmail.com>
2026-05-06 12:54:00 +0530

    Delete and Update of each student

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('students/', views.studentsView),
+    path('student/<int:pk>',views.studentDetailView),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentsView(request):
     #students = Student.objects.all()
     #students_list = list(students.values())
@@ -15,4 +15,36 @@ def studentsView(request):
         students = Student.objects.all()
         serializer = StudentSerializer(students,many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        print(serializer.error)
+        return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk = pk)
+    except Student.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status =status.HTTP_200_OK)
+        else:
+            return Response(serializer.error, status = status.HTTP_404_NOT_FOUND)
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+    
+        
+
+
+    
 
